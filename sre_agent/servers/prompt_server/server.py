10a810f9afc7d12346a26fa01ebc61f2f7da2378
Scott Clare <scott@fuzzylabs.ai>
2025-04-30 09:30:01 +0100

    feat: paramaterising org name and repo name

--- sre_agent/servers/prompt_server/server.py
+++ sre_agent/servers/prompt_server/server.py
@@ -1,9 +1,47 @@
 """A server containing a prompt to trigger the agent."""
 
+import os
+from dataclasses import dataclass, fields
+from functools import lru_cache
+from typing import TYPE_CHECKING
+
+from dotenv import load_dotenv
 from mcp.server.fastmcp import FastMCP
+
+if TYPE_CHECKING:
+    from _typeshed import DataclassInstance
 
 mcp = FastMCP("sre-agent-prompt")
 mcp.settings.port = 3001
+
+load_dotenv()
+
+
+def _validate_fields(self: DataclassInstance) -> None:
+    for config in fields(self):
+        attr = getattr(self, config.name)
+
+        if not attr:
+            msg = f"Environment variable {config.name.upper()} is not set."
+            raise ValueError(msg)
+
+
+@dataclass(frozen=True)
+class PromptServerConfig:
+    """A config class containing Github org and repo name environment variables."""
+
+    organisation: str = os.getenv("GITHUB_ORGANISATION", "")
+    repo_name: str = os.getenv("GITHUB_REPO_NAME", "")
+    project_root: str = os.getenv("PROJECT_ROOT", "")
+
+    def __post_init__(self) -> None:
+        """A post-constructor method for the dataclass."""
+        _validate_fields(self)
+
+
+@lru_cache
+def _get_prompt_server_config() -> PromptServerConfig:
+    return PromptServerConfig()
 
 
 @mcp.prompt()
@@ -13,10 +51,11 @@ def diagnose(service: str, channel_id: str) -> str:
     {service} service, I only want you to check the pods logs, look up only the 1000
     most recent logs. Feel free to scroll up until you find relevant errors that
     contain reference to a file, once you have these errors and the file name, get the
-    file contents of the path src for the repository microservices-demo in the
-    organisation fuzzylabs. Keep listing the directories until you find the file name
-    and then get the contents of the file. Once you have diagnosed the error please
-    report this to the following slack channel: {channel_id}."""
+    file contents of the path {_get_prompt_server_config().project_root} for the
+    repository {_get_prompt_server_config().repo_name} in the organisation
+    {_get_prompt_server_config().organisation}. Keep listing the directories until you
+    find the file name and then get the contents of the file. Once you have diagnosed
+    the error please report this to the following slack channel: {channel_id}."""
 
 
 if __name__ == "__main__":
