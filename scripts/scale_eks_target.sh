d67027be089d01b7ad628a415fefecd4828c7a2c
chris <32800386+Christopher-Norman@users.noreply.github.com>
2025-05-06 16:37:10 +0100

    Change desired target cluster nodes back to two

--- scripts/scale_eks_target.sh
+++ scripts/scale_eks_target.sh
@@ -10,11 +10,11 @@ if [ "$1" == "--down" ]; then
     --nodegroup-name "$NODEGROUP_NAME" \
     --scaling-config minSize=0,maxSize=1,desiredSize=0
 elif [ "$1" == "--up" ]; then
-  echo "Scaling node group '$NODEGROUP_NAME' up to 1..."
+  echo "Scaling node group '$NODEGROUP_NAME' up to 2..."
   aws eks update-nodegroup-config \
     --cluster-name "$CLUSTER_NAME" \
     --nodegroup-name "$NODEGROUP_NAME" \
-    --scaling-config minSize=0,maxSize=2,desiredSize=1
+    --scaling-config minSize=0,maxSize=2,desiredSize=2
 else
   echo "Usage: $0 [--up | --down]"
   exit 1
