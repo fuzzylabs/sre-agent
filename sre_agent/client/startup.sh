e152f392709a5a6fe6afa383233a50d63f27f9dc
chris <32800386+Christopher-Norman@users.noreply.github.com>
2025-05-08 16:19:06 +0100

    Fix orchestrator startup

--- sre_agent/client/startup.sh
+++ sre_agent/client/startup.sh
@@ -1,6 +1,26 @@
 python3 -c "
-from transformers import AutoModel
-model = AutoModel.from_pretrained('meta-llama/Llama-Prompt-Guard-2-86M')
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+)
+import os
+
+# Define the model name before using it
+model_name = 'meta-llama/Llama-Prompt-Guard-2-86M'
+
+if not os.environ.get('HF_HOME'):
+    os.environ['HF_HOME'] = '~/.cache/huggingface'
+
+model_path = os.path.expanduser(
+    os.path.join(os.environ['HF_HOME'], model_name.replace('/', '--'))
+)
+
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+# Save the model and tokenizer locally
+model.save_pretrained(model_path)
+tokenizer.save_pretrained(model_path)
 "
 
 llamafirewall configure
